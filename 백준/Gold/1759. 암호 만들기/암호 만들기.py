8aee7a4c52ae2e1a2c61268731bd9400fe6da81e
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-09-05 02:37:26 +0900

    [Gold V] Title: 암호 만들기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,60 +1,23 @@
+# 입력 받기
 from itertools import combinations
 
-# 입력 받기
 L, C = map(int, input().split())
-arr = list(input().split())
+arr = input().split()
 
-# 최소 한개의 모음, 두개의 자음이라는 조건
-
-# 암호 후보가 될 문자를 모두 뽑은 후, 정렬해서 출력하기
-
-# 모음, 자음 각각 추출
-vowels = ['a', 'e', 'i', 'o', 'u']
-
-mo = []
-ja = []
-for c in arr:
-    if c in vowels:
-        mo.append(c)
-    else:
-        ja.append(c)
-
-
-# 자음 후보 리스트 생성
-ja_comb = list(combinations(ja, 2))
-
-# 최종 암호 문자열 리스트
+# 모든 가능한 조합 다 뽑고 거기서 최소 1개의 모음, 2개의 자음이 없는 애들은 거르기
+all_list = list(combinations(sorted(arr), L))
 result = []
+for elem in all_list:
+    # 모음 개수와 자음 개수 세기
+    vowel_count = 0
+    consonant_count = 0
+    for char in elem:
+        if char in "aeiou":
+            vowel_count += 1
+        else:
+            consonant_count += 1
 
-for m in range(len(mo)):
-    for j in range(len(ja_comb)):
-        picked = []
-
-        # 모음 리스트에서 일단 1개 뽑기
-        picked.append(mo[m])
-
-        # 자음 후보 리스트에서 일단 2개 뽑기
-        picked.append(ja_comb[j][0])
-        picked.append(ja_comb[j][1])
-        # print(picked)
-
-        # 나머지 모음+자음에서 남은 개수만큼 뽑기
-        # 나머지 문자 후보 리스트 생성
-        other = arr[:]
-        other.remove(mo[m])
-        other.remove(ja_comb[j][0])
-        other.remove(ja_comb[j][1])
-
-        other_comb = list(combinations(other, L-3))
-        for elem in other_comb:
-            # 최종 문자 선택 리스트 생성
-            final_picked = picked+list(elem)
-            final_picked.sort()
-            total = ''.join(final_picked)
-            if total not in result:
-                result.append(total)
-
-
-result.sort()
-for elem in result:
-    print(elem)
+    # 모음이 최소 1개, 자음이 최소 2개인 경우만 처리
+    if vowel_count >= 1 and consonant_count >= 2:
+        # 조합을 문자열로 변환하여 결과 리스트에 추가
+        print(''.join(elem))
