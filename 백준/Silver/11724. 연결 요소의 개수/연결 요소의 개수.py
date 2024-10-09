5eb33a48fedc3021ebf582b43544176aa4245947
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-10-09 20:27:50 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 65288 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,37 @@
 import sys
-sys.setrecursionlimit(5000)
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
 
-def dfs(graph, v, visited):
-    visited[v]=True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph,i,visited)
+# 입력 받기
+N, M = map(int, input().split())
 
-N,M = map(int, sys.stdin.readline().rstrip().split())
-graph = [[] for _ in range(N+1)]
+matrix = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    v1,v2 = map(int,sys.stdin.readline().rstrip().split())
-    graph[v1].append(v2)
-    graph[v2].append(v1)
+    y, x = map(int, input().split())
+    matrix[y].append(x)
+    matrix[x].append(y)
 
-visited = [False]* (N+1)
+# print(matrix)
 
-count=0
 
-for i in range(1,N+1):
-    if not visited[i]:
-        if not graph[i]:
-            count+=1
-            visited[i]=True
-        else:
-            dfs(graph,i,visited)
-            count+=1
+visited = [False]*(N+1)
 
-print(count)
+
+def dfs(x):
+    global visited, matrix
+    visited[x] = True
+
+    for i in matrix[x]:
+        if not visited[i]:
+            dfs(i)
+
+
+cnt = 0
+for x in range(1, N+1):
+    if not visited[x]:
+        dfs(x)
+        cnt += 1
+
+
+print(cnt)
