b8000ba46e9fcf92bc4efecd52cc17ebe6bc854a
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-11-06 17:48:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 87.42 ms, Memory: 13.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    stack = []
+    
+    for num in number:
+        while k>0 and stack and stack[-1] < num:
+            stack.pop()
+            k-=1
+        stack.append(num)
+    
+    if(k!=0): # k가 남아 있는 경우
+        stack = stack[:-k]
+    
+        
+    
+    return ''.join(stack)
