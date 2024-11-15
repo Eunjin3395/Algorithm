1fc949833e11a1dfc18eea69f50a019da1373fe3
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-11-15 09:23:25 +0900

    [level 3] Title: N으로 표현, Time: 17.02 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,23 @@
+def solution(N, number):
+    answer = -1
+    if number == N:
+        return 1
+    
+    _li = [set() for i in range(8)]
+    for i in range(len(_li)):
+        _li[i].add(int(str(N)*(i+1)))
+        
+    for i in range(1,8):
+        for j in range(i):
+            for op1 in _li[j]:
+                for op2 in _li[i-j-1]:
+                    _li[i].add(op1+op2)
+                    _li[i].add(op1-op2)
+                    _li[i].add(op1*op2)
+                    if op2 != 0:
+                        _li[i].add(op1//op2)
+        if number in _li[i]:
+            answer = i+1
+            break
+    
+    return answer
