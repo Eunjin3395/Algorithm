049f0283f28e550e84f9ae22207df110ac684bb4
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-11-18 07:45:01 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,46 +1,25 @@
-import sys
+# 입력 받기
+T = int(input())
 
-# ( -> push
-# ) -> pop
-class Stack:
-    def __init__(self):
-        self.stack=[]
-        self.isVPS=True
+for i in range(T):
+    string = input()
 
-    def push(self):
-        self.stack.append(1)
+    num = 0
+    isVPS = True
 
+    for s in string:
+        if s == "(":
+            num += 1
+        else:
+            num -= 1
 
-    def pop(self):
-        if self.stack: # stack에 요소 남아 있는 경우
-            self.stack.pop()
-        else: # stack이 비었는데 pop하려는 경우 VPS가 아님
-            self.isVPS=False
+        if num < 0:
+            isVPS = False
+            break
+    if not num == 0:
+        isVPS = False
 
-    def isValid(self):
-        if self.stack: # 모든 인덱스 돌고도 stack에 요소 남아 있는 경우 VPS가 아님
-            self.isVPS=False
-        
-        return self.isVPS
-
-
-
-n = int(sys.stdin.readline())
-
-for i in range(n):
-    string=sys.stdin.readline()
-
-    stack = Stack()
-
-    for k in range(len(string)):
-        if(string[k]=="("):
-            stack.push()
-        elif(string[k]==")"):
-            stack.pop()
-    
-    isVPS=stack.isValid()
-    if(isVPS):
+    if isVPS:
         print("YES")
     else:
         print("NO")
-
