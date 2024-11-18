addb140b7b6fe74804e8fd7d78bbf4b59cbe11f7
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-11-18 18:26:52 +0900

    [Gold IV] Title: 최단경로, Time: 772 ms, Memory: 73672 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,4 +1,4 @@
-from queue import PriorityQueue
+import heapq
 import sys
 input = sys.stdin.readline
 
@@ -16,17 +16,21 @@ for _ in range(E):
     u, v, w = map(int, input().split())
     adj_list[u].append([w, v])  # dist, next_node
 
-pq = PriorityQueue()
-pq.put([0, start_node])
+q = []
+heapq.heappush(q, [0, start_node])  # q에 [dist,next_node] 추가
 dist[start_node] = 0
 
-while not pq.empty():
-    curr_dist, curr_node = pq.get()
+while q:
+    curr_dist, curr_node = heapq.heappop(q)
+    # 탐색 최적화
+    if dist[curr_node] < curr_dist:
+        continue
+
     for adj_dist, adj_node in adj_list[curr_node]:
         temp_dist = curr_dist+adj_dist
         if(temp_dist < dist[adj_node]):
             dist[adj_node] = temp_dist
-            pq.put([temp_dist, adj_node])
+            heapq.heappush(q, [temp_dist, adj_node])
 
 
 for elem in dist[1:]:
