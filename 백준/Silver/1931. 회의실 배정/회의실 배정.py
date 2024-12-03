8564cfdf60c88c03bf2f60e5ddafb96a0511452c
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-12-04 07:50:36 +0900

    [Silver I] Title: 회의실 배정, Time: 260 ms, Memory: 59852 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,21 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 # 입력 받기
 N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-arr = []
-
-
-for _ in range(N):
-    start, end = map(int, input().split())
-    arr.append([start, end])
+# print(arr)
 
 sorted_arr = sorted(arr, key=lambda x: (x[1], x[0]))
+# print(sorted_arr)
 
 result = 0
 endTime = 0
 
 for time in sorted_arr:
-    if(time[0] >= endTime):
-        result += 1
+    if time[0] >= endTime:
         endTime = time[1]
+        result += 1
 
 print(result)
