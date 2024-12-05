8689b4eb3b051467c9782371cdee84a71f388432
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2024-12-06 07:40:06 +0900

    [Silver III] Title: 1로 만들기, Time: 480 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,34 @@
-x = int(input())
+# 입력 받기
+N = int(input())
 
-d = [0] * (x+1)
+# INF = int(1e6)
 
-for i in range(2,x+1):
-    d[i]=d[i-1]+1
+if N == 1:
+    print(0)
+    exit()
+if N == 2:
+    print(1)
+    exit()
+if N == 3:
+    print(1)
+    exit()
 
-    if(i % 3 == 0):
-        d[i] = min(d[i],d[i//3]+1)
+dp = [0 for _ in range(N+1)]
 
-    if(i % 2 == 0):
-        d[i]=min(d[i],d[i//2]+1)
-    
+# dp[x]: x -> 1을 만들기 위한 연산 최소 횟수
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, N+1):
+    if i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i//3], dp[i//2], dp[i-1])+1
+    elif i % 3 == 0:
+        dp[i] = min(dp[i//3], dp[i-1])+1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i//2], dp[i-1])+1
+    else:
+        dp[i] = dp[i-1]+1
 
 
-print(d[x])
+print(dp[N])
