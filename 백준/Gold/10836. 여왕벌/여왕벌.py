790305a972614417e182104e8ae68d264caa6e0d
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-01-13 15:47:03 +0900

    [Gold III] Title: 여왕벌, Time: 2644 ms, Memory: 36632 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 M, N = map(int, input().split())
 
 matrix = [[1 for _ in range(M)] for _ in range(M)]
@@ -10,28 +7,20 @@ matrix = [[1 for _ in range(M)] for _ in range(M)]
 
 # print(matrix)
 
-# 제일 왼쪽 열과 위쪽 행의 숫자 list -> 시간 초과
+# 제일 왼쪽 열과 위쪽 행의 숫자 list
 arr = [1 for _ in range(2*M-1)]
 
 for _ in range(N):
-    zero, one, two = list(map(int, input().split()))
+    input_arr = list(map(int, input().split()))
 
-    for i in range(zero, zero+one):
-        arr[i] += 1
-
-    for i in range(zero+one, zero+one+two):
-        arr[i] += 2
-
-# # 1 더하기 시작 idx와 종료 idx, 2 더하기 시작 idx와 종료 idx를 매 입력마다 저장 (N번)
-# idxs = []
-# for _ in range(N):
-#     zero, one, two = list(map(int, input().split()))
-#     idxs.append([zero, zero+one, one, one+two])
-
-# # 4*N 크기의 인덱스 matrix가 나오면 그걸 한번에 계산해서 arr로 만들기
-# print(idxs)
-# arr=[1 for _ in range(2*M-1)]
-
+    n = 0
+    idx = 0
+    for num in input_arr:
+        if num:
+            for i in range(idx, idx+num):
+                arr[i] += n
+            idx += num
+        n += 1
 
 # print(arr)
 
@@ -55,7 +44,7 @@ for i in range(M-1, 2*M-1):
 # 나머지 matrix dp 채우기
 for y in range(1, M):
     for x in range(1, M):
-        matrix[y][x] = max(matrix[y-1][x-1], matrix[y-1][x])
+        matrix[y][x] = max(matrix[y][x-1], matrix[y-1][x-1], matrix[y-1][x])
 
 for row in matrix:
     for elem in row:
