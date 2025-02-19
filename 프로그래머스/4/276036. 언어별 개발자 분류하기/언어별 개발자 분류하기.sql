281a16a8faa802cfd058a27fc5162b6c0c7c3f21
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-02-20 02:49:32 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,24 @@
+WITH skill_mapping AS (
+SELECT 
+    BIT_OR(CASE WHEN CATEGORY = 'Front End' THEN CODE ELSE 0 END) AS FE,
+    BIT_OR(CASE WHEN NAME = 'Python' THEN CODE ELSE 0 END) AS PY,
+    BIT_OR(CASE WHEN NAME = 'C#' THEN CODE ELSE 0 END) AS CS
+FROM SKILLCODES
+)
+
+select 
+    case
+        when d.skill_code & s.fe > 0 and d.skill_code & s.py > 0 
+        then 'A'
+        when d.skill_code & s.cs > 0
+        then 'B'
+        else 'C'
+        end as GRADE,
+        ID, 
+        EMAIL
+from DEVELOPERS d
+cross join skill_mapping s
+where (d.skill_code & s.fe > 0 and d.skill_code & s.py > 0)
+    or d.skill_code & s.cs > 0
+    or d.skill_code & s.fe > 0
+order by grade, id
