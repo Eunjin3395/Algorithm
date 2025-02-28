d8b68e609b4dbf2489afa8c50d116ef3c3628590
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-03-01 03:04:44 +0900

    [Gold V] Title: 연속합 2, Time: 140 ms, Memory: 44996 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,35 +1,23 @@
-N = int(input())
+import sys
+input = sys.stdin.readline
 
-arr = list(map(int, input().split()))
+# 입력 받기
+n = int(input().strip())
+arr = list(map(int, input().strip().split()))
 
-if N == 1:
-    print(arr[0])
-    exit()
+# DP 배열 초기화
+dp_no_remove = [0] * n
+dp_remove = [0] * n
 
-# 수를 삭제 했는지 여부도 같이 저장해야된다
-# dp[x][0] = x번째까지 수열에서 수를 제거하지 않은 경우 최대 연속합
-# dp[x][0] = x번째까지 수열에서 수를 제거한 경우 최대 연속합
+# 초기값 설정
+dp_no_remove[0] = arr[0]
+dp_remove[0] = arr[0]
+max_sum = arr[0]
 
-INF = -1e8
+# DP 진행
+for i in range(1, n):
+    dp_no_remove[i] = max(dp_no_remove[i-1] + arr[i], arr[i])
+    dp_remove[i] = max(dp_remove[i-1] + arr[i], dp_no_remove[i-1])
+    max_sum = max(max_sum, dp_no_remove[i], dp_remove[i])
 
-dp = [[INF, INF] for _ in range(N)]
-
-dp[0][0] = arr[0]
-dp[1][0] = max(arr[0] + arr[1], arr[1])
-dp[1][1] = max(arr[0], arr[1])
-# print(dp)
-
-# dp[x][0] = x-1번째까지 제거 안한 합 + x번째, x번째만
-# dp[x][1] = x-1번째까지 제거 안한 합, x-1번째까지 제거한 합 + x번째
-
-for i in range(2, N):
-    dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i])
-    dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i])
-
-# print(dp)
-
-mx = INF
-for a, b in dp:
-    mx = max(mx, a, b)
-
-print(mx)
+print(max_sum)
