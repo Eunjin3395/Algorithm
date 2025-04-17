ed5c7b8a47a166c5ad612dc789bd3ca0a8cbca7e
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-04-17 22:30:40 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,68 +1,26 @@
+from collections import deque
 import sys
+input = sys.stdin.readline
 
-class Dequeue:
-    def __init__(self):
-        self.deq = []
-    
-    def push_front(self,x):
-        self.deq.insert(0,x)
+N = int(input())
+q = deque()
 
-    def push_back(self,x):
-        self.deq.append(x)
+for _ in range(N):
+    cmd = list(input().split())
+    if cmd[0] == "push_front":
+        q.appendleft(int(cmd[1]))
+    elif cmd[0] == "push_back":
+        q.append(int(cmd[1]))
+    elif cmd[0] == "pop_front":
+        print(q.popleft() if q else -1)
+    elif cmd[0] == "pop_back":
+        print(q.pop() if q else -1)
+    elif cmd[0] == "size":
+        print(len(q))
+    elif cmd[0] == "empty":
+        print(0 if q else 1)
+    elif cmd[0] == "front":
+        print(q[0] if q else -1)
+    else:
+        print(q[-1] if q else -1)
 
-    def pop_front(self):
-        if self.deq:
-            return self.deq.pop(0)
-        else:
-            return -1
-    
-    def pop_back(self):
-        if self.deq:
-            return self.deq.pop()
-        else:
-            return -1
-        
-    def size(self):
-        return len(self.deq)
-    
-    def empty(self):
-        return (0 if self.deq else 1)
-    
-    def front(self):
-        if self.deq:
-            return self.deq[0]
-        else:
-            return -1
-        
-    def back(self):
-        if self.deq:
-            return self.deq[-1]
-        else:
-            return -1
-        
-
-n = int(sys.stdin.readline())
-
-deq=Dequeue()
-
-for i in range(n):
-    command = sys.stdin.readline().rstrip()
-
-    if("push_front" in command):
-        x = int(command.split()[1])
-        deq.push_front(x)
-    elif("push_back" in command):
-        x = int(command.split()[1])
-        deq.push_back(x)
-    elif("pop_front"in command):
-        print(deq.pop_front())
-    elif("pop_back" in command):
-        print(deq.pop_back())
-    elif("size" in command):
-        print(deq.size())
-    elif("empty" in command):
-        print(deq.empty())
-    elif("front" in command):
-        print(deq.front())
-    elif("back" in command):
-        print(deq.back())
