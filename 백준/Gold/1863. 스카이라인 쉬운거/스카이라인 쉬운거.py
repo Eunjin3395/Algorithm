afa444962bf5af3f499aeaf408fc73af9a36e535
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-04-17 11:22:30 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -1,32 +1,21 @@
 import sys
 input = sys.stdin.readline
+
 N = int(input())
-height = [list(map(int, input().split())) for _ in range(N)]
-
-
-# stack
-# 입력 받은게 stack의 마지막 높이보다 높으면 stack에 추가
-# 작으면 stack의 마지막 높이가 같거나 빌 때까지 pop
 stack = []
-answer = 0
-for idx, cord in enumerate(height):
-    x, y = cord[0], cord[1]
-    if stack:
-        if y > stack[-1]:
-            stack.append(y)
-        elif y < stack[-1]:
-            while stack and stack[-1] >= y:
-                k = stack.pop()
-                if k > y:
-                    answer += 1
-            stack.append(y)
-            # print("answer:", answer)
-    else:
-        stack.append(y)
-    # print("idx:", idx, ", y:", y, "stack: ", stack)
+count = 0
 
-while stack:
-    n = stack.pop()
-    if n > 0:
-        answer += 1
-print(answer)
+for _ in range(N):
+    x, y = map(int, input().split())
+
+    while stack and stack[-1] > y:
+        stack.pop()
+        count += 1
+
+    if not stack or stack[-1] < y:
+        if y != 0:
+            stack.append(y)
+
+# 남아있는 높이 처리
+count += len(stack)
+print(count)
