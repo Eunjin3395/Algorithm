5159fdbdd827218fb45f9f0a47d089b4478409b4
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-04-18 00:52:53 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,55 +1,25 @@
+from collections import deque
 import sys
-import queue
+input = sys.stdin.readline
 
-class Queue:
-    def __init__(self):
-        self.queue=[]
-    
-    def push(self,x):
-        self.queue.append(x)
+N = int(input())
+q = deque()
 
-    def pop(self):
-        if (self.queue):
-            return self.queue.pop(0)
+for _ in range(N):
+    cmd = list(input().split())
+    if cmd[0] == "push":
+        q.append(int(cmd[1]))
+    elif cmd[0] == "pop":
+        if q:
+            print(q.popleft())
         else:
-            return -1
-    
-    def size(self):
-        return len(self.queue)
-    
-    def empty(self):
-        return (0 if self.queue else 1)
-    
-    def front(self):
-        if(self.queue):
-            return self.queue[0]
-        else:
-            return -1
-        
-    def back(self):
-        if(self.queue):
-            return self.queue[-1]
-        else:
-            return -1
-        
+            print(-1)
+    elif cmd[0] == "size":
+        print(len(q))
+    elif cmd[0] == "empty":
+        print(0 if q else 1)
+    elif cmd[0] == "front":
+        print(q[0] if q else -1)
+    elif cmd[0] == "back":
+        print(q[-1] if q else -1)
 
-n = int(sys.stdin.readline())
-
-q= Queue()
-
-for i in range(n):
-    command = sys.stdin.readline().rstrip()
-
-    if("push" in command):
-        x = int(command.split()[1])
-        q.push(x)
-    elif("pop" in command):
-        print(q.pop())
-    elif("size" in command):
-        print(q.size())
-    elif("empty" in command):
-        print(q.empty())
-    elif("front" in command):
-        print(q.front())
-    elif("back" in command):
-        print(q.back())
