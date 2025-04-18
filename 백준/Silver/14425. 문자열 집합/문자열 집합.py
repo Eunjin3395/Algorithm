d8bacf2a5f87fb359b319eeb1cd9350f100b9d8a
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-04-18 10:08:10 +0900

    [Silver IV] Title: 문자열 집합, Time: 580 ms, Memory: 37432 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+
+_set = set()
+for _ in range(N):
+    _set.add(input())
+
+answer = 0
+for _ in range(M):
+    if input() in _set:
+        answer += 1
+
+print(answer)
