305dce1035c788d1bca425dd0c12f73c8d810503
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-04-18 22:27:06 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -1,16 +1,13 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-dp = [0]*N
-
-# dp[x]:x번째까지의 합
-dp[0] = A[0]
-
+# dp
+# dp[x]: x를 포함하는 증가하는 부분 수열의 합
+dp = [x for x in A]
 for i in range(1, N):
     for j in range(i):
         if A[i] > A[j]:
-            dp[i] = max(dp[i], dp[j]+A[i])
-        else:
-            dp[i] = max(dp[i], A[i])
+            dp[i] = max(dp[i], dp[j] + A[i])
 
+# print(dp)
 print(max(dp))
