7112e57888695dda8a55f8ef4e574c9dc514aa30
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-06-20 08:52:20 +0900

    [Gold IV] Title: 점프, Time: 1656 ms, Memory: 44460 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -11,6 +11,7 @@ for _ in range(M):
 # dp
 # 2차원 풀이
 # X를 N으로 설정하니까 메모리 초과
+# 어차피 한번에 갈 수 있는 최대 칸은 N보다 훨씬 작음
 X = 150
 dp = [[INF] * X for _ in range(N + 1)]
 dp[2][1] = 1  # 제일 처음엔 한 칸 점프만 가능
@@ -27,20 +28,20 @@ def print_dp():
 for y in range(3, N + 1):
     if banned[y]:  # 금지된 돌
         continue
-    for x in range(1, X):
-        if y - x <= 0:  # 1칸 이상 점프해야함
+    for x in range(1, X):  # 1칸 이상 점프해야함함
+        if y - x <= 0:  #
             continue
         if banned[y - x]:
             continue
 
         val = INF
-        if y > x:
-            if x - 1 > 0 and x - 1 < X:
-                val = min(val, dp[y - x][x - 1])
-            if x > 0:
-                val = min(val, dp[y - x][x])
-            if x + 1 > 0 and x + 1 < X:
-                val = min(val, dp[y - x][x + 1])
+        if x - 1 > 0 and x - 1 < X:
+            val = min(val, dp[y - x][x - 1])
+        if x > 0:
+            val = min(val, dp[y - x][x])
+        if x + 1 > 0 and x + 1 < X:
+            val = min(val, dp[y - x][x + 1])
+
         if val < INF:
             dp[y][x] = val + 1
 
