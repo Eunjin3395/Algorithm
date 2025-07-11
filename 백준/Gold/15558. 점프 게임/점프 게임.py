437eed4fb550aaf07596b94732abd4641225f58f
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-07-11 12:05:42 +0900

    [Gold V] Title: 점프 게임, Time: 248 ms, Memory: 49184 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.py
+++ 백준/Gold/15558. 점프 게임/점프 게임.py
@@ -13,35 +13,24 @@ visited = [[False] * N for _ in range(2)]
 q = deque()
 q.append((0, 0, 0))  # left/right, n, time
 
-dk = [[0, 0, 1], [0, 0, -1]]
-dn = [[1, -1, K], [1, -1, K]]
-
-
+dk = [[0, 0, 1], [0, 0, -1]]  # 왼쪽 / 오른쪽
+dn = [[1, -1, K], [1, -1, K]]  # 위 1칸, 아래 1칸, 위 K칸
 
 while q:
     k, n, t = q.pop()
-    # print("curr:", k, n, t)
-
-    if n >= N:
-        print("1")
-        exit()
 
     for i in range(3):
         nk = k + dk[k][i]
         nn = n + dn[k][i]
 
-
         if 0 <= nk <= 1 and 0 <= nn:
-            # print("next node:", nk, nn, t + 1)
             if nn > t:
                 if nn >= N:
                     print("1")
                     exit()
 
                 if matrix[nk][nn] == "1" and not visited[nk][nn]:
-                    # print("node append:", nk, nn, t + 1)
                     q.append((nk, nn, t + 1))
                     visited[nk][nn] = True
 
 print("0")
-
