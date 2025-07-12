1aac28458d464bab7a41214b616ada65ac5e7541
Eunjin Kim <114724403+Eunjin3395@users.noreply.github.com>
2025-07-12 20:06:02 +0900

    [Silver I] Title: 절댓값 힙, Time: 328 ms, Memory: 30620 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public void solve() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int N = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) > Math.abs(o2)) {
+                    return Math.abs(o1) - Math.abs(o2);
+                } else if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solve();
+    }
+}
