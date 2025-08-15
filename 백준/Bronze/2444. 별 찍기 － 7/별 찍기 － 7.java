dfbe4409c69b215d1404d6c048d8fbd602156337
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-08-15 16:04:21 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 124 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 0; j < N - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solve();
+    }
+}
