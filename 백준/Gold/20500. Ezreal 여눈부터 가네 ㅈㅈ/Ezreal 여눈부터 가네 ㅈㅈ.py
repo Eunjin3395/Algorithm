ef577d592e38fbee9e65e8c2e267dc0a57c66d53
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-08-20 08:11:36 +0900

    [Gold V] Title: Ezreal 여눈부터 가네 ㅈㅈ, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/20500. Ezreal 여눈부터 가네 ㅈㅈ/Ezreal 여눈부터 가네 ㅈㅈ.py
@@ -0,0 +1,11 @@
+N = int(input())
+MOD = 1000000007
+dp = [[0 for _ in range(3)] for _ in range(N + 1)]
+dp[1][1] = 1
+
+for i in range(2, N + 1):
+    dp[i][0] = dp[i - 1][1] + dp[i - 1][2]
+    dp[i][1] = dp[i - 1][0] + dp[i - 1][2]
+    dp[i][2] = dp[i - 1][0] + dp[i - 1][1]
+
+print(dp[N][0] % MOD)
