d9771c6b12cfd881bc43eed3ea1b33b9db0c7031
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-09-24 23:08:55 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION = 'Sick'
-ORDER BY ANIMAL_ID
+select ANIMAL_ID, NAME
+from animal_ins
+where intake_condition = 'Sick'
+order by 1
