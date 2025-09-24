ca8d2f951ffa0cedd41c9dd736d99cc7326073db
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-09-24 23:36:49 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION != 'Aged'
-ORDER BY ANIMAL_ID
+select ANIMAL_ID, NAME
+from animal_ins
+where INTAKE_CONDITION !='Aged'
+order by 1
