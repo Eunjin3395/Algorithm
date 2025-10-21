c1609d1d8dfe740a048aacb2f8dedab896ce1886
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-10-21 09:19:48 +0900

    [Silver V] Title: BABBA, Time: 124 ms, Memory: 15992 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[2][46];
+        dp[0][2] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for (int i = 3; i <= K; i++) {
+            dp[0][i] = dp[0][i - 2] + dp[0][i - 1];
+            dp[1][i] = dp[1][i - 2] + dp[1][i - 1];
+        }
+        System.out.print(dp[0][K] + " " + dp[1][K]);
+    }
+}
